b939bdf72332b0e05f2ac9f16d43f6a94ce0e88d
Harmin00 <63843513+Harmin00@users.noreply.github.com>
2020-04-17 22:49:38 +0530

    Update file.js

--- file.js
+++ file.js
@@ -2,8 +2,8 @@
 myObj = [{ Name: "Chilly", Price: 30.00 },
 { Name: "AavinBrown", Price: 26.50 },
 { Name: "aarokiyaMilk", Price: 30.00 },
-         { Name: "Carrot", Price: 30.00 },
-         { Name: "Beans", Price: 45.00 },
-         { Name: "Onion", Price: 20.00 },
-         { Name: "tomato", Price: 10.00 }
+{ Name: "Carrot", Price: 30.00 },
+{ Name: "Beans", Price: 45.00 },
+{ Name: "Onion", Price: 20.00 },
+{ Name: "tomato", Price: 10.00 }
 ];
