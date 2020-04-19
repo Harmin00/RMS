93fe7f27111bbc2055c7ac528c3d9f54e6158a02
Harmin00 <63843513+Harmin00@users.noreply.github.com>
2020-04-19 07:45:05 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,11 +1,11 @@
 ﻿var myObj, x;
 myObj = [
- {Name :"தனியா  100GM" , Price :25.00},
-{Name :"மக்கா சோளம்  100GM" , Price :32.00},
-{Name :"சீரகம்  100GM" , Price :12.00},
-{Name :"சுக்கு  100GM" , Price :68.00},
-{Name :"கன்டந்திப்பிலி  100GM" , Price :41.00},
-{Name :"சுக்கு  100GM" , Price :3.50},
-{Name :"கிஸ்மிஸ்  100GM" , Price :89.00},
-{Name :"பெருஞ்சீரகம்  100GM" , Price :25.00}
+ {Name :"100GM" , Price :25.00},
+{Name :" 100GM" , Price :32.00},
+{Name :"100GM" , Price :12.00},
+{Name :"100GM" , Price :68.00},
+{Name :"100GM" , Price :41.00},
+{Name :"100GM" , Price :3.50},
+{Name :"100GM" , Price :89.00},
+{Name :" 100GM" , Price :25.00}
 ];
